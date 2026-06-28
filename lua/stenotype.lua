6da30f48f17c431602bf9d7b33e144c0d8128330
Chenlin <1chen0bryan@gmail.com>
2026-06-27 20:15:37 -0700

    Formatting: {^.}

--- lua/stenotype.lua
+++ lua/stenotype.lua
@@ -1,7 +1,55 @@
-local function output(text, env)
-	local front_pad = " "
+local key_sim = require("ydotool")
 
-	env.engine:commit_text(front_pad..text)
+local function format_output(text, env)
+	local front_pad = ""
+	if env.space_before then front_pad = " " end
+
+	local back_pad = ""
+	if env.space_after then back_pad = " " end
+
+	env.space_before = env.engine.context:get_option("space_before")
+	env.space_after = env.engine.context:get_option("space_after")
+
+	return front_pad .. text .. back_pad
+end
+
+local output_funcs = {}
+
+output_funcs["{^[^^]+}"] = function(text, env)
+	if env.prev_output:match(" $") then
+		key_sim.press_key("backspace")
+	end
+
+	env.space_before = false
+	return format_output(text:gsub("^{^", ""):gsub("}$", ""), env)
+end
+
+local function output(text, env)
+	env.prev_text = text
+	local out = text
+
+	if output_funcs[text] then
+		out = output_funcs[text](text, env)
+		goto output
+	end
+
+	for pat, func in pairs(output_funcs) do
+		if text:match(pat) then
+			out = func(text, env)
+			goto output
+		end
+	end
+
+	out = format_output(out, env)
+
+	::output::
+	if not out then
+		env.prev_output = ""
+		return
+	end
+
+	env.prev_output = out
+	env.engine:commit_text(out)
 end
 
 local function query_w_limit(trans, input, seg, limit)
@@ -25,6 +73,10 @@ local T = {}
 
 function T.init(env)
 	env.tran = Component.TableTranslator(env.engine, "translator", "table_translator")
+	env.prev_text = ""
+	env.prev_output = ""
+	env.space_before = env.engine.context:get_option("space_before")
+	env.space_after = env.engine.context:get_option("space_after")
 end
 
 function T.fini(env)
@@ -33,10 +85,11 @@ end
 function T.func(input, seg, env)
 	if not input:match("/$") then return end
 
+	local context = env.engine.context
+
 	local strokes = input
 	local extra = nil
 
-	::query::
 	local cands = query_w_limit(env.tran, strokes, seg, 9)
 	if #cands == 0 then
 		strokes = input:gsub("/[^/]+/$", "/")
@@ -51,18 +104,18 @@ function T.func(input, seg, env)
 		if extra then
 			cands = query_w_limit(env.tran, extra, seg, 9)
 		else
-			env.engine.context:clear()
+			context:clear()
 			return
 		end
 	end
 
 	if #cands == 1 and cands[1].comment == "" then
-		env.engine.context:clear()
+		context:clear()
 		output(cands[1].text, env)
 	else
 		if extra then
-			env.engine.context:clear()
-			env.engine.context:push_input(extra)
+			context:clear()
+			context:push_input(extra)
 		end
 		for _, cand in ipairs(cands) do
 			yield(cand)
@@ -70,4 +123,24 @@ function T.func(input, seg, env)
 	end
 end
 
-return { tran=T }
+local P = {}
+
+function P.init(env)
+	env.sending_key = false
+end
+
+function P.fini(env)
+end
+
+function P.func(key, env)
+	local sending_key = env.engine.context:get_option("sending_key")
+
+	if sending_key then
+		env.engine.context:set_option("sending_key", false)
+		return 0
+	end
+
+	return 2
+end
+
+return { tran = T, proc = P }
---
+++ lua/ydotool.lua
@@ -0,0 +1,21 @@
+keycode_map = require("input_event_codes")
+
+local function get_keycode(key)
+	if type(key) == "number" then
+		return key
+	elseif type(key) == "string" then
+		return keycode_map[key:upper()]
+	end
+
+	return nil
+end
+
+local M = {}
+
+function M.press_key(key)
+	local code = get_keycode(key)
+	if not code then return end
+	os.execute(string.format("ydotool key %d:1 %d:0", code, code))
+end
+
+return M
